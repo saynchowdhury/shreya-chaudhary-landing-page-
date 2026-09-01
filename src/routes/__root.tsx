0e11da4df4095fdadd200a65754c65e7afd5d8ac
sayan  chowdhury <sayanshytech.20@gmail.com>
2026-09-02 03:06:50 +0530

    prevent_mobile_horizontal_overflow

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -130,7 +130,7 @@ function RootShell({ children }: { children: ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body>
+      <body className="overflow-x-hidden">
         {children}
         <Scripts />
       </body>
